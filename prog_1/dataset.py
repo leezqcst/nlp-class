ee0031a26540900bfdba2560e007b28a5f67b86a
pratheek ms <pratheekonline@gmail.com>
2014-08-10 23:41:35 +0530

    tabs to spaces

--- prog_1/dataset.py
+++ prog_1/dataset.py
@@ -9,65 +9,65 @@ output = []
 tweet_output = []
 d = defaultdict(int)
 tokens = (
-		'NUMBER',
-		'SNAME',
-		'RT',
-		'HASHTAG',
-		'URL',
-		'SLANG',
-		'WORD',
-		'SINGLEQUOTE',
-		'DOUBLEQUOTE',
-		'EXCLAMATION',
-		'QUESTION',
-		'EMOTICON',
-		'PUNCTUATION',
-		'PERIOD'
-		)
+        'NUMBER',
+        'SNAME',
+        'RT',
+        'HASHTAG',
+        'URL',
+        'SLANG',
+        'WORD',
+        'SINGLEQUOTE',
+        'DOUBLEQUOTE',
+        'EXCLAMATION',
+        'QUESTION',
+        'EMOTICON',
+        'PUNCTUATION',
+        'PERIOD'
+        )
 t_ignore  = ' \t'
 
 def t_NUMBER(t) :
-	r'\d+' 
+    r'\d+' 
 
 def t_SNAME(t) :
-	r'@\w+'
-	d['name'] += 1
+    r'@\w+'
+    d['name'] += 1
 
 def t_HASHTAG(t) :
-	r'\#\w+'
-	d['hashtag'] += 1
+    r'\#\w+'
+    d['hashtag'] += 1
 
 def t_RT(t) :
-	r'^RT'
-	d['retweet'] += 1
-	
+    r'^RT'
+    d['retweet'] += 1
+    
 def t_URL(t):
-	r'https?'
-	d['url'] += 1
+    r'https?'
+    d['url'] += 1
 
 def t_SLANG(t):
-	r'\w+'
-	slangs = ['LOL','ROFL','ROFLMAO','DM','ICYMI','CC','NTS','NP','AFAIK','BTW','DP']
-	if (t.value).upper() in slangs:
-		d['slang'] += 1
+    r'\w+'
+    slangs = ['LOL','ROFL','ROFLMAO','DM','ICYMI','CC','NTS','NP','AFAIK','BTW','DP']
+    if (t.value).upper() in slangs:
+        d['slang'] += 1
 
 def t_EMOTICON(t):
-	r'[\:;\-\(\)DPpd]{2,}'
-	d['emoticon'] += 1
+    r'[\:;\-\(\)DPpd]{2,}'
+    d['emoticon'] += 1
 
 def t_error(t):
-	t.lexer.skip(1)
-	
+    t.lexer.skip(1)
+    
 lexer = lex.lex()
 soln = open('twitter.csv','wb')
 writer = csv.writer(soln, delimiter=',')
 for i,line in enumerate(open('Dataset9.txt')):
-	line = line.strip()
-	if i%2 == 0:
-		continue # skip any blank line
-	global out
-	lexer.input(line)
-	lexer.token()
-	out = [d['hashtag'], d['retweet'], d['url'], d['name'], d['slang'], d['emoticon']]
-	writer.writerow(out)
-	d = defaultdict(int)
+    line = line.strip()
+    if i%2 == 0:
+        continue # skip any blank line
+    global out
+    lexer.input(line)
+    lexer.token()
+    out = [d['hashtag'], d['retweet'], d['url'], d['name'], d['slang'], d['emoticon']]
+    writer.writerow(out)
+    d = defaultdict(int)
