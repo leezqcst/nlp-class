949b9179b44ea2f72615b1cfb6c85207f600156d
pratheek ms <pratheekonline@gmail.com>
2014-09-13 09:37:04 +0530

    fix brigram trigram

--- mini_proj_2/bigram.py
+++ mini_proj_2/bigram.py
@@ -1,16 +1,24 @@
 import nltk
 from math import log
+from collections import defaultdict
 import re
+words = nltk.word_tokenize("~ "+open("corpus1.txt").read().lower()+" ~")
+count = defaultdict(int)
+count_bigram = defaultdict(int)
 
-words = nltk.word_tokenize(open("corpus2.txt").read().lower())
 cleaned_words = []
-punctuation = [',', '.', '?', '!', ':', ';', '\'', '\"','@', '=', '&', '(', ')' ]
+punctuation = [',', '.', '?', '!', ':', ';', '\'', '\"','@', '=', '&', '(', ')' ,'[', ']']
 for word in words:
 	if(not (word in punctuation)):
-		cleaned_words.append(re.sub(r'[,.\']','',word))
+		word = re.sub(r'[,.\'`]','',word)
+		if(len(word) > 0):
+			cleaned_words.append(word)
+			count[word] += 1
 
-bigrams = [(cleaned_words[x], cleaned_words[x+1]) for x in range(len(cleaned_words) -1)]
+for i in range(len(cleaned_words)-1):
+	count_bigram[(cleaned_words[i],cleaned_words[i+1])] += 1
 
-for bigram in bigrams:
-    q_ML = 1.0*bigrams.count(bigram)/cleaned_words.count(bigram[0])
-    print bigram[0]+","+bigram[1]+","+str(q_ML)+","+str(log(q_ML))+"\n"
+for key,value in count_bigram.iteritems():
+	q_ML = float(value)/count[key[0]]
+	print key[0]+","+key[1]+","+str(q_ML)+","+str(log(q_ML))
+
--- mini_proj_2/trigram.py
+++ mini_proj_2/trigram.py
@@ -1,18 +1,24 @@
 import nltk
 from math import log
 import re
+from collections import defaultdict
 
-words = nltk.word_tokenize(open("corpus2.txt").read().lower())
+words = nltk.word_tokenize("~ ~ "+open("corpus2.txt").read().lower()+" ~ ~")
 cleaned_words = []
 punctuation = [',', '.', '?', '!', ':', ';', '\'', '\"','@', '=', '&', '(', ')' ]
+two_words = defaultdict(int)
+three_words = defaultdict(int)
+
 for word in words:
 	if(not (word in punctuation)):
-		cleaned_words.append(re.sub(r'[,.\']','',word))
+		word = re.sub(r'[,.\'`]','',word)
+		if(len(word) > 0):
+			cleaned_words.append(word)
 
-bigrams = [(cleaned_words[x], cleaned_words[x+1]) for x in range(len(cleaned_words) -1)]
+for i in range(len(cleaned_words)-2):
+	two_words[(cleaned_words[i], cleaned_words[i+1])] += 1
+	three_words[(cleaned_words[i], cleaned_words[i+1], cleaned_words[i+2])] += 1
 
-trigrams = [(cleaned_words[x], cleaned_words[x+1], cleaned_words[x+2]) for x in range(len(cleaned_words) - 2)]
-
-for trigram in trigrams:
-    q_ML = float(trigrams.count(trigram))/bigrams.count((trigram[0], trigram[1]))
-    print trigram[0]+","+trigram[1]+","+trigram[2]+","+str(q_ML)+","+str(log(q_ML))+"\n"
+for key,value in three_words.iteritems():
+	q_ML = float(value)/two_words[(key[0],key[1])]
+	print key[0]+","+key[1]+","+key[2]+","+str(q_ML)+","+str(log(q_ML))
