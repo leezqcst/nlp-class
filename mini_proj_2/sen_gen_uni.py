29e4eb481ebb68760661c1db2ae73c976af63aeb
pratheek ms <pratheekonline@gmail.com>
2014-09-14 14:27:32 +0530

    sentence generation partial

--- mini_proj_2/sen_gen_uni.py
+++ mini_proj_2/sen_gen_uni.py
@@ -1,10 +1,16 @@
 import nltk
 from collections import defaultdict
 import re
+import random
 from math import log
 
 words = nltk.word_tokenize(open("corpus1.txt").read().lower())
 uni = defaultdict(int)
+
+stop = set()
+for sentence in open("corpus1.txt").read().lower().split("."):
+	if(len(sentence) > 0):
+		stop.add(sentence.split(" ")[-1])
 
 punctuation = [',', '.', '?', '!', ':', ';', '\'', '\"','@', '=', '&' ]
 count = 0
@@ -15,5 +21,12 @@ for word in words:
 		count += 1
 
 for key,value in uni.iteritems():
-	probab = (1.0*value)/count
-	print key+","+str(probab)+","+str(log(probab))+"\n"
+	uni[key] = (1.0*value)/count
+
+i = 0
+while (True):
+	word = random.choice(uni.keys())
+	if(i > 7 and word in stop):
+		break
+	print word+" ",
+	i = i+1
